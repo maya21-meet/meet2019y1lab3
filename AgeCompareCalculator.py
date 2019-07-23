5e2183aec8e5ee03b930f1732eff414ca6dd0c1d
maya21-meet <maya21@meet.mit.edu>
2019-07-23 10:25:26 -0400

    Stopping here

---
+++ AgeCompareCalculator.py
@@ -0,0 +1,10 @@
+person_one=input('what is the name of the first person?')
+print(person_one)
+person_two=input('what is the name of the second person?')
+print(person_two)
+age1= input('what is the age of the first person?')
+print(age1)
+
+
+
+IT DOESNT WORK!
